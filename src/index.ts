ac11e177713e2d13dee06b55c7d25741a93d0d24
armfazh <armfazh@cloudflare.com>
2023-07-31 00:32:46 -0700

    Starting privacypass-ts library.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Copyright (c) 2023 Cloudflare, Inc.
+// Licensed under the Apache-2.0 license found in the LICENSE file or at https://opensource.org/licenses/Apache-2.0
+
+// Privacy Pass Issuance Protocol - draft 11
+// https://datatracker.ietf.org/doc/draft-ietf-privacypass-protocol/11/
+
+export {  };
