8e72c09f896c5f29701505bd4178cd2c30107f93
Thibault Meunier <thibault@cloudflare.com>
2026-06-05 10:15:24 +0200

    test header_to_token output

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 // Copyright (c) 2023 Cloudflare, Inc.
 // Licensed under the Apache-2.0 license found in the LICENSE file or at https://opensource.org/licenses/Apache-2.0
 
-import { base64url } from 'rfc4648';
 import { WWWAuthenticateHeader } from './auth_scheme/private_token.js';
 import {
     Client as PublicVerifClient,
